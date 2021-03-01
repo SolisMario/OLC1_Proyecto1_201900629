c3e3efc6932b5f7711d6604b170567df0153f577
Mario <solis.mariojosue@gmail.com>
2021-03-01 11:05:05 -0600

    Se agregó métodos  para método del árbol

--- OLC1_Proyecto1_201900629/src/analizadores/Arbol.java
+++ OLC1_Proyecto1_201900629/src/analizadores/Arbol.java
@@ -132,4 +132,394 @@ public class Arbol {
             escribir_rec(raiz.derecho, nombreDot);
         }
     }
+    
+    public static void setAnulables(Nodo raiz) {
+        Nodo tmp = raiz;
+        if (tmp != null) {
+            setAnulables(tmp.izquierdo);
+            if (tmp.identificador != null) {
+                tmp.anulable = "N";
+            } else {
+                reglaAnulable(tmp);
+            }
+            setAnulables(tmp.derecho);
+        }
+    }
+
+    public static void reglaAnulable(Nodo actual) {
+        switch (actual.simbolo) {
+            case "*":
+                actual.anulable = "A";
+                break;
+            case "+":
+                if (actual.derecho.anulable.equals("A")) {
+                    actual.anulable = "A";
+                } else {
+                    actual.anulable = "N";
+                }
+                break;
+            case "?":
+                actual.anulable = "A";
+                break;
+            case "|":
+                if (actual.izquierdo.anulable.equals("A") || actual.derecho.anulable.equals("A")) {
+                    actual.anulable = "A";
+                } else {
+                    actual.anulable = "N";
+                }
+                break;
+            case ".":
+                if (actual.izquierdo.anulable.equals("A") && actual.derecho.anulable.equals("A")) {
+                    actual.anulable = "A";
+                } else {
+                    actual.anulable = "N";
+                }
+                break;
+        }
+    }
+
+    public static void setPrimeros(Nodo raiz) {
+        Nodo tmp = raiz;
+        if (tmp != null) {
+            setPrimeros(tmp.izquierdo);
+            setPrimeros(tmp.derecho);
+            if (tmp.identificador != null) {
+                tmp.primeros = tmp.identificador;
+            } else {
+                reglaPrimeros(tmp);
+            }
+        }
+    }
+
+    public static void reglaPrimeros(Nodo actual) {
+        switch (actual.simbolo) {
+            case "*":
+                actual.primeros = actual.derecho.primeros;
+                break;
+            case "+":
+                actual.primeros = actual.derecho.primeros;
+                break;
+            case "?":
+                actual.primeros = actual.derecho.primeros;
+                break;
+            case "|":
+                actual.primeros = actual.izquierdo.primeros + "," + actual.derecho.primeros;
+                break;
+            case ".":
+                if (actual.izquierdo.anulable.equals("A")) {
+                    actual.primeros = actual.izquierdo.primeros + "," + actual.derecho.primeros;
+                } else {
+                    actual.primeros = actual.izquierdo.primeros;
+                }
+                break;
+        }
+    }
+
+    public static void setUltimos(Nodo raiz) {
+        Nodo tmp = raiz;
+        if (tmp != null) {
+            setUltimos(tmp.izquierdo);
+            setUltimos(tmp.derecho);
+            if (tmp.identificador != null) {
+                tmp.ultimos = tmp.identificador;
+            } else {
+                reglaUltimos(tmp);
+            }
+        }
+    }
+
+    public static void reglaUltimos(Nodo actual) {
+        switch (actual.simbolo) {
+            case "*":
+                actual.ultimos = actual.derecho.ultimos;
+                break;
+            case "+":
+                actual.ultimos = actual.derecho.ultimos;
+                break;
+            case "?":
+                actual.ultimos = actual.derecho.ultimos;
+                break;
+            case "|":
+                actual.ultimos = actual.izquierdo.ultimos + "," + actual.derecho.ultimos;
+                break;
+            case ".":
+                if (actual.derecho.anulable.equals("A")) {
+                    actual.ultimos = actual.izquierdo.ultimos + "," + actual.derecho.ultimos;
+                } else {
+                    actual.ultimos = actual.derecho.ultimos;
+                }
+                break;
+        }
+    }
+
+    public static void getSiguientes(Arbol arbol, Nodo raiz) {
+        Nodo tmp = raiz;
+        if (tmp != null) {
+            getSiguientes(arbol, tmp.izquierdo);
+            if (tmp.simbolo.equals(".") || tmp.simbolo.equals("*") || tmp.simbolo.equals("+")) {
+                reglaSiguientes(arbol, tmp);
+            }
+            getSiguientes(arbol, tmp.derecho);
+        }
+    }
+
+    public static void reglaSiguientes(Arbol arbol, Nodo actual) {
+        if (actual.simbolo.equals(".")) {
+            String[] ultimosC1 = actual.izquierdo.ultimos.split(",");
+            String[] primerosC2 = actual.derecho.primeros.split(",");
+
+            for (int i = 0; i < ultimosC1.length; i++) {
+                int ultimo = Integer.parseInt(ultimosC1[i]);
+                int indexUltimo = ultimo - 1;
+                for (int j = 0; j < primerosC2.length; j++) {
+                    arbol.siguientes.set(indexUltimo, arbol.siguientes.get(indexUltimo) + primerosC2[j] + ",");
+                }
+            }
+        } else {
+            String[] ultimosC1 = actual.derecho.ultimos.split(",");
+            String[] primerosC1 = actual.derecho.primeros.split(",");
+
+            for (int i = 0; i < ultimosC1.length; i++) {
+                int ultimo = Integer.parseInt(ultimosC1[i]);
+                int indexUltimo = ultimo - 1;
+                for (int j = 0; j < primerosC1.length; j++) {
+                    arbol.siguientes.set(indexUltimo, arbol.siguientes.get(indexUltimo) + primerosC1[j] + ",");
+                }
+            }
+        }
+    }
+
+    public static void esteticaSiguientes(Arbol arbol) {
+        for (int i = 0; i < arbol.siguientes.size() - 1; i++) {
+            String siguientes = arbol.siguientes.get(i);
+            siguientes = siguientes.replaceFirst(".$", "");
+            arbol.siguientes.set(i, siguientes);
+        }
+    }
+
+    public static void esteticaTransiciones(Arbol arbol) {
+        for (int i = 0; i < arbol.transiciones.size(); i++) {
+            String transicion = arbol.transiciones.get(i);
+            transicion = transicion.replaceFirst(".$", "");
+            arbol.transiciones.set(i, transicion);
+        }
+    }
+
+    public static void tablaTransiciones(Arbol arbol) {
+        int identificadorEstados = 1;
+        String estadoInicial = "S0|" + arbol.root.primeros + "|";
+        arbol.transiciones.add(estadoInicial);
+        for (int i = 0; i < arbol.transiciones.size(); i++) {
+            String[] fila = arbol.transiciones.get(i).split("\\|");
+
+            String nombre = fila[0];
+
+            String[] identificadores = fila[1].split(",");
+
+            for (int j = 0; j < arbol.alfabeto.size() - 1; j++) {
+                String transicionAlfabeto = new String();
+                for (int k = 0; k < identificadores.length; k++) {
+                    int identificadorIndex = Integer.parseInt(identificadores[k]) - 1;
+                    String simboloIdentificador = arbol.siguientes.get(identificadorIndex).split("\\|")[0];
+                    if (simboloIdentificador.replaceAll("\"", "").equals(arbol.alfabeto.get(j).replaceAll("\"", ""))) {
+                        transicionAlfabeto += arbol.siguientes.get(identificadorIndex).split("\\|")[2] + ",";
+                    }
+                }
+
+                String subconjunto = crearSubconjuntos(transicionAlfabeto);
+                if (!subconjunto.equals("")) {
+                    subconjunto = ordenarSubconjunto(subconjunto);
+                    String verifSubconjunto = verificarSubconjunto(subconjunto, arbol.transiciones);
+                    if (verifSubconjunto.equals("")) {
+                        String estado = "S" + String.valueOf(identificadorEstados);
+                        arbol.transiciones.add(estado + "|" + subconjunto + "|");
+                        String transActual = arbol.transiciones.get(i);
+                        arbol.transiciones.set(i, transActual + estado + "~" + arbol.alfabeto.get(j) + ",");
+                        identificadorEstados += 1;
+                    } else {
+                        String transActual = arbol.transiciones.get(i);
+                        arbol.transiciones.set(i, transActual + verifSubconjunto + "~" + arbol.alfabeto.get(j) + ",");
+                    }
+                }
+            }
+        }
+        esteticaTransiciones(arbol);
+        getAceptacion(arbol);
+    }
+
+    public static String crearSubconjuntos(String todos) {
+        LinkedList<String> estadoLimpio = new LinkedList<>();
+        String[] todosSiguientes = todos.split(",");
+        for (int i = 0; i < todosSiguientes.length; i++) {
+            boolean existente = false;
+            if (!estadoLimpio.contains(todosSiguientes) && !todosSiguientes[i].equals("")) {
+                estadoLimpio.add(todosSiguientes[i]);
+            }
+        }
+        String subconjunto = new String();
+        for (int i = 0; i < estadoLimpio.size(); i++) {
+            if (i != estadoLimpio.size() - 1) {
+                subconjunto += estadoLimpio.get(i) + ",";
+            } else {
+                subconjunto += estadoLimpio.get(i);
+            }
+        }
+        return subconjunto;
+    }
+
+    public static String ordenarSubconjunto(String subconjunto) {
+        String subOrdenado = "";
+        String[] siguientesStr = subconjunto.split(",");
+        int[] siguientesInt = new int[siguientesStr.length];
+        for (int i = 0; i < siguientesStr.length; i++) {
+            if (!siguientesStr[i].equals("")) {
+                siguientesInt[i] = Integer.parseInt(siguientesStr[i]);
+            }
+        }
+        int size = siguientesInt.length;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (siguientesInt[j] > siguientesInt[j + 1]) {
+                    int temp = siguientesInt[j];
+                    siguientesInt[j] = siguientesInt[j + 1];
+                    siguientesInt[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            if (i < size - 1) {
+                subOrdenado += String.valueOf(siguientesInt[i]) + ",";
+            } else {
+                subOrdenado += String.valueOf(siguientesInt[i]);
+            }
+        }
+        return subOrdenado;
+    }
+
+    public static String verificarSubconjunto(String subconjunto, LinkedList<String> transiciones) {
+        String estadoHallado = "";
+        for (int i = 0; i < transiciones.size(); i++) {
+            String conjunto = transiciones.get(i).split("\\|")[1];
+            if (subconjunto.equals(conjunto)) {
+                estadoHallado = transiciones.get(i).split("\\|")[0];
+                break;
+            }
+        }
+        return estadoHallado;
+    }
+
+    public static void getAlfabeto(Arbol arbol, Nodo raiz) {
+        Nodo tmp = raiz;
+        if (tmp != null) {
+            getAlfabeto(arbol, tmp.izquierdo);
+            if (tmp.identificador != null) {
+                if(!arbol.alfabeto.contains(tmp.simbolo)){
+                    arbol.alfabeto.add(tmp.simbolo);
+                }
+            }
+            getAlfabeto(arbol, tmp.derecho);
+        }
+    }
+
+    public static void getAceptacion(Arbol arbol) {
+        LinkedList<String> transiciones = arbol.transiciones;
+        String idAceptacion = arbol.root.derecho.identificador;
+        for (int i = 0; i < transiciones.size(); i++) {
+            String[] transicion = transiciones.get(i).split("\\|");
+            String estadoActual = transicion[0];
+            String composestado = transicion[1];
+            if (composestado.endsWith(idAceptacion)) {
+                arbol.estadosAceptacion.add(estadoActual);
+            }
+        }
+    }
+
+    public static void graficarAFD(Arbol arbol) throws IOException {
+        LinkedList<String> tablaTrancisiones = arbol.transiciones;
+        LinkedList<String> estadosAceptacion = arbol.estadosAceptacion;
+
+        String nombreDot = arbol.nombreRegex + "AFD.dot";
+        String nombrePng = arbol.nombreRegex + "AFD.png";
+
+        File archivo = new File(nombreDot);
+        FileWriter fw = new FileWriter(archivo);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        bw.write("digraph afd {\n");
+        bw.write("rankdir=LR;\n");
+
+        for (int i = 0; i < tablaTrancisiones.size(); i++) {
+            String estado = tablaTrancisiones.get(i).split("\\|")[0];
+            bw.write("\"" + estado + "\"" + "[shape=circle,label=\"" + estado + "\"]\n");
+        }
+
+        for (int i = 0; i < estadosAceptacion.size(); i++) {
+            String estado = estadosAceptacion.get(i).split("\\|")[0];
+            bw.write("\"" + estado + "\"" + "[shape=doublecircle,label=\"" + estado + "\"]\n");
+        }
+
+        for (int i = 0; i < tablaTrancisiones.size(); i++) {
+            System.out.println(tablaTrancisiones.get(i));
+            String[] transicion = tablaTrancisiones.get(i).split("\\|");
+            String estadoActual = transicion[0];
+            String[] estadosSiguientes;
+            if (transicion.length > 2) {
+                estadosSiguientes = transicion[2].split(",");
+                for (int j = 0; j < estadosSiguientes.length; j++) {
+                    String[] composTransicion = estadosSiguientes[j].split("~");
+                    bw.write("\"" + estadoActual + "\"" + "->" + "\"" + composTransicion[0] + "\"" + "[label="  + composTransicion[1] + "]\n");
+                }
+            }
+        }
+
+        bw.write("vacio[shape=none, style=invisible]\n");
+        bw.write("vacio -> S0[label=" + "\"" + "Inicio" + "\"" + "]\n");
+
+        bw.write("}");
+        bw.close();
+
+        Runtime.getRuntime().exec("dot -Tpng " + nombreDot + " -o " + nombrePng);
+
+    }
+
+    public static void reporteSiguienes(Arbol arbol) throws IOException {
+        LinkedList<String> tablaSiguientes = arbol.siguientes;
+
+        String nombreDot = arbol.nombreRegex + "Sig.dot";
+        String nombrePng = arbol.nombreRegex + "sig.png";
+
+        File archivo = new File(nombreDot);
+        FileWriter fw = new FileWriter(archivo);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        bw.write("digraph tablaSiguientes {\n");
+        bw.write("abc [shape=none, margin=0, label=<\n");
+        bw.write("<TABLE BORDER=" + "\"" + 0 + "\"" + " CELLBORDER=" + "\"" + 1 + "\"" + " CELLSPACING=" + "\"" + 0 + "\"" + " CELLPADDING=" + "\"" + 4 + "\"" + ">\n");
+        bw.write("<TR>\n");
+        bw.write("<TD COLSPAN=" + "\"" + 2 + "\"" + ">Hojas</TD>\n");
+        bw.write("<TD>Siguientes</TD>\n");
+        bw.write("</TR>\n");
+        for (int i = 0; i < tablaSiguientes.size(); i++) {
+            if (i < tablaSiguientes.size() - 1) {
+                String[] filaTabla = tablaSiguientes.get(i).split("\\|");
+                bw.write("<TR>\n");
+                bw.write("<TD>" + filaTabla[0] + "</TD>\n");
+                bw.write("<TD>" + filaTabla[1] + "</TD>\n");
+                bw.write("<TD>" + filaTabla[2] + "</TD>\n");
+                bw.write("</TR>\n");
+            } else {
+                String[] filaTabla = tablaSiguientes.get(i).split("\\|");
+                bw.write("<TR>\n");
+                bw.write("<TD>" + filaTabla[0] + "</TD>\n");
+                bw.write("<TD>" + filaTabla[1] + "</TD>\n");
+                bw.write("<TD>--</TD>\n");
+                bw.write("</TR>\n");
+            }
+        }
+        bw.write("</TABLE>>];\n");
+        bw.write("}");
+        bw.close();
+
+        Runtime.getRuntime().exec("dot -Tpng " + nombreDot + " -o " + nombrePng);
+    }
 }
